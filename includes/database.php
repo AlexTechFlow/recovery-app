8ec050c8eb20650eeb64c1a0fe4b2421f1f542f9
AlexTechFlow <designdigitalhealth@gmail.com>
2018-11-18 19:24:24 -0500

    Uploading project files to remote repo on Github

---
+++ includes/database.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$dsn = "mysql:host=localhost;alexbeli_app";
+
+$db_username = "alexbeli_usr3";
+$db_password = "Alfan9x77xMrk";
+
+$pdo = new PDO($dsn, $db_username, $db_password);
+
+
+
+?>
